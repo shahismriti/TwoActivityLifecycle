e51507bde709e6595b4b9163af4d5be4a3ae0fa8
Smriti Shahi <smritishahi42@gmail.com>
2022-04-05 20:14:59 +0545

    'activitylifecyclechallenge

---
+++ app/src/main/java/com/example/activitylifecyclechallenge/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.activitylifecyclechallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.example.android.activitylifecyclechallenge.extra.REPLY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void addItem(View view) {
+        String item = ((Button)view).getText().toString();
+        Intent itemIntent = new Intent();
+        itemIntent.putExtra(EXTRA_REPLY, item);
+        setResult(RESULT_OK, itemIntent);
+        finish();
+    }
+}
